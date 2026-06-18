9f40ae17bbcd14c462e793dfc2b8a49ff3bcfc48
sachinshrmaa <krisa1826@gmail.com>
2026-06-18 20:58:58 +0530

    update: description

--- lib/utils.ts
+++ lib/utils.ts
@@ -17,5 +17,5 @@ export const SITE_URL = 'https://wicklog.in';
 export const BLOG_URL = `${SITE_URL}/blog`;
 export const SITE_NAME = 'Wicklog';
 export const BLOG_TITLE = 'Wicklog Blog';
-export const BLOG_DESCRIPTION = 'Writing about finance, investing, and building products.';
+export const BLOG_DESCRIPTION = 'Product updates, how-to guides, and insights to help you trade smarter.';
 export const BLOG_AUTHOR = 'Sachin Sharma';
